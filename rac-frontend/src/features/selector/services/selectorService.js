6f66d924e1ba538459a8cc1ee2fc934032ea56a7
Harshita Kashyap <harshitakp.123@gmail.com>
2026-04-20 18:12:39 +0530

    working on selector dashboard

---
+++ rac-frontend/src/features/selector/services/selectorService.js
@@ -0,0 +1,5 @@
+// 4)src/features/selector/pages/SelectorCandidateDetail.jsx
+// 5)src/features/selector/components/SelectorRibbon.jsx
+// 6)src/features/selector/pages/SelectorCandidates.jsx
+// 7)src/features/selector/services/selectorService.js
+// empty file
